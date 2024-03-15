114160cd71439dbcc556266da3515a774bf00920
Ariady Putra <ariady.putra@gmail.com>
2024-03-15 09:38:31 +0700

    Expose className

--- src/components/AnchorScrollOffset.tsx
+++ src/components/AnchorScrollOffset.tsx
@@ -1,18 +1,13 @@
 AnchorScrollOffset.defaultProps = {
-  navbarHeight: 20,
+  className: "-top-20", // navbar height
 };
 /**
  * Offsets to NavBar height.
  * @param props id: string
  * @returns AnchorScrollOffset
  */
-function AnchorScrollOffset(props: { id: string }) {
-  return (
-    <div
-      id={props.id}
-      className="relative -top-20" // navbar height
-    />
-  );
+function AnchorScrollOffset(props: { id: string; className: string }) {
+  return <div id={props.id} className={`relative ${props.className}`} />;
 }
 
 export default AnchorScrollOffset;
