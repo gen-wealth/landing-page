98568da85f0e85ec1fe3319b42745ea1f1b50eed
Ariady Putra <ariady.putra@gmail.com>
2024-03-18 17:14:07 +0700

    Proper Roadmap theming

--- src/components/home/Roadmap.tsx
+++ src/components/home/Roadmap.tsx
@@ -8,7 +8,6 @@ type status = "Completed" | "In progress" | "Planned";
 type roadmap = {
   time: string;
   title: string;
-  // paragraphs: JSX.Element[];
   status: status;
 };
 
@@ -125,22 +124,22 @@ function Roadmap() {
             {roadmaps.map((roadmap, i) => (
               <li
                 key={roadmap.title}
-                className={`step sm:!min-w-96 before:!shadow-xl after:!shadow-xl before:contrast-150 after:contrast-150
+                className={`step sm:!min-w-96 before:!shadow-xl after:!shadow-xl
                 max-sm:before:!-translate-y-full max-sm:before:scale-x-150 max-sm:before:mt-28 after:!place-self-start max-sm:after:mt-7 after:mx-auto
                 sm:before:!-translate-x-full sm:before:scale-y-150 sm:before:!ml-10 sm:after:!justify-self-start sm:after:!m-1
                 ${
                   roadmap.status === "Completed"
-                    ? "before:!bg-[#340b64] before:!saturate-50 after:!bg-[#340b64] after:!saturate-50 after:!text-[rgb(196,181,202)]"
-                    : "step-primary before:!bg-[rgb(196,181,202)] before:saturate-150 after:!bg-[rgb(196,181,202)] after:saturate-150 after:!text-[#340b64]"
+                    ? "before:!bg-neutral after:!bg-neutral after:!text-neutral-content"
+                    : "step-primary before:!bg-accent after:!bg-accent after:!text-accent-content"
                 }
                 ${theme === "sun" ? "before:brightness-150 after:brightness-150" : ""}`}
                 data-content={roadmapStatus[roadmap.status]}
               >
-                <div className="text-start text-indigo-950 place-self-start mb-10">
+                <div className="text-start text-secondary place-self-start mb-10">
                   <AnchorScrollOffset id={"roadmap-" + i} />
                   <AnchorScrollOffset id={"roadmap-" + i + "-portrait"} className="-top-20 -left-12" />
                   <time className="font-mono italic">{roadmap.time}</time>
-                  <div className="text-lg text-purple-950 font-black">
+                  <div className="text-lg text-primary font-black">
                     <a
                       // id={"roadmap-" + i}
                       onClick={(e) => {
@@ -162,11 +161,6 @@ function Roadmap() {
                       {roadmap.title}
                     </a>
                   </div>
-                  {/* {roadmap.paragraphs.map((paragraph, i) => (
-                    <div key={`${roadmap.title}.${i}`} className="text-indigo-950 my-2 style-p">
-                      {paragraph}
-                    </div>
-                  ))} */}
                   <FormattedMessage id={roadmapStatusLangKey[roadmap.status]} />
                 </div>
               </li>
