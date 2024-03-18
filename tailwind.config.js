c675bf28df92891e13f59224e615707b1ac25569
Ariady Putra <ariady.putra@gmail.com>
2024-03-18 19:59:41 +0700

    sun & moon Themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,14 +20,27 @@ module.exports = {
       // "synthwave",
       {
         moon: {
-          primary: "#3b0764",
-          secondary: "#1e1b4b",
+          primary: "#3b0764", // purple-950
+          secondary: "#1e1b4b", // indigo-950
           accent: "#f9c3ff",
           neutral: "#180733",
           "base-100": "#1a103d",
-          info: "#10a9fe",
-          success: "#a9fe10",
-          warning: "#fea910",
+          info: "#9a65b1",
+          success: "#9ad52b",
+          warning: "#f1a20f",
+          error: "#fe3210",
+        },
+      },
+      {
+        sun: {
+          primary: "#3b0764", // purple-950
+          secondary: "#1e1b4b", // indigo-950
+          accent: "#fff",
+          neutral: "#220b4a",
+          "base-100": "#1a103d",
+          info: "#9a65b1",
+          success: "#9ad52b",
+          warning: "#f1a20f",
           error: "#fe3210",
         },
       },
