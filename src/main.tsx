eb936d06c80e48da2d445cd0376622712010e0c2
Ariady Putra <ariady.putra@gmail.com>
2024-03-19 14:42:55 +0700

    Decouple IntlProvider from Home page component

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import Home from "./pages/Home.tsx";
 import "./styles/index.css";
 import { getLocale } from "./util.ts";
 import ThemeProvider from "./components/ThemeProvider.tsx";
+import { IntlProvider } from "react-intl";
 
 const locale = getLocale();
 fetch(`/lang/${locale}.json`).then((lang) => {
   lang.json().then((messages) => {
     ReactDOM.createRoot(document.getElementById("root")!).render(
       <React.StrictMode>
-        <ThemeProvider>
-          <Home locale={locale} messages={messages} />
-        </ThemeProvider>
+        <IntlProvider locale={locale} messages={messages}>
+          <ThemeProvider>
+            <Home />
+          </ThemeProvider>
+        </IntlProvider>
       </React.StrictMode>
     );
   });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,8 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
 import HomeContents from "../components/home/HomeContents";
-import IntlProvider from "react-intl/src/components/provider";
 
-Home.defaultProps = {
-  locale: "en",
-  messages: {},
-};
-function Home(props: { locale: string; messages: Record<string, string> }) {
+function Home() {
   // Scroll element into view from pathname as anchor on window load
   useEffect(() => {
     const id = location.pathname.slice(1);
@@ -27,7 +22,7 @@ function Home(props: { locale: string; messages: Record<string, string> }) {
   }, []);
 
   return (
-    <IntlProvider locale={props.locale} messages={props.messages}>
+    <>
       <NavBar />
       <Hero />
 
@@ -38,7 +33,7 @@ function Home(props: { locale: string; messages: Record<string, string> }) {
       </div>
 
       <Footer />
-    </IntlProvider>
+    </>
   );
 }
 
