397eaf6f92e7a5f42cb78c89a43483821202ebf8
Ariady Putra <ariady.putra@gmail.com>
2024-03-20 19:48:00 +0700

    minor

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -1,7 +1,7 @@
 function Svg(props: { w?: number; h?: number; className?: string; filter?: string; xlinkHref: string }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={props.w} height={props.h} viewBox="0 0 40 40" className={props.className} filter={props.filter}>
-      <use xlinkHref={props.xlinkHref}></use>
+      <use xlinkHref={props.xlinkHref} />
     </svg>
   );
 }
