7f759f7c44108710a1c4a0c9907dd13f840abec5
Ariady Putra <ariady.putra@gmail.com>
2024-03-28 22:01:54 +0700

    minor adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,26 +13,26 @@ function Footer(props: { className?: string }) {
         className={`footer max-sm:flex max-sm:flex-col items-center p-4 rounded-box m-2 w-auto shadow-xl
         ${props.className ?? "bg-neutral bg-opacity-[calc(2/3)] text-neutral-content"}`}
       >
-        <aside className="items-center flex flex-wrap sm:flex-nowrap">
+        <aside className="items-center text-center flex flex-wrap sm:flex-nowrap">
           <a
             // href="#"
             onClick={scrollToTop}
-            className="flex max-sm:flex-col gap-2 items-center mx-auto"
+            className="flex max-sm:flex-col gap-2 max-sm:gap-0 font-bold items-center mx-auto"
           >
             <div className="btn btn-ghost btn-circle hover:bg-opacity-0">
               <img alt={title} src="/GenWealth.ico" className="w-12 mask mask-squircle" />
             </div>
 
-            <h5 className="max-sm:w-full font-bold text-center text-sm">{title}</h5>
+            <h5 className="text-sm max-sm:text-xl max-sm:w-full">{title}</h5>
 
-            <span className="max-sm:hidden text-xs">&laquo;</span>
-            <h6 className="max-sm:w-full font-bold text-center text-sm">{intl.formatMessage({ id: "tagLine" })}</h6>
-            <span className="max-sm:hidden text-xs">&raquo;</span>
+            <span className="text-xs max-sm:hidden">&laquo;</span>
+            <h6 className="text-sm max-sm:text-xs max-sm:w-full">{intl.formatMessage({ id: "tagLine" })}</h6>
+            <span className="text-xs max-sm:hidden">&raquo;</span>
           </a>
 
-          <p className="max-sm:w-full text-center text-xs">{intl.formatMessage({ id: "copyright" })}</p>
+          <p className="text-xs max-sm:w-full">{intl.formatMessage({ id: "copyright" })}</p>
         </aside>
-        <nav className="grid-flow-col gap-4 max-sm:place-self-center sm:justify-self-end">
+        <nav className="grid-flow-col gap-4 sm:justify-self-end max-sm:place-self-center max-sm:-mt-6">
           {/* <ThemeController
             className="m-auto"
             sun={{
@@ -47,16 +47,16 @@ function Footer(props: { className?: string }) {
             }}
           /> */}
           <a href="https://twitter.com/genwealth_app" aria-label="Twitter">
-            <Svg xlinkHref="#social-twitter" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-twitter" w={24} h={24} className="fill-current" />
           </a>
           <a href="https://www.linkedin.com/company/genwealth-app" aria-label="LinkedIn">
-            <Svg xlinkHref="#social-linkedin" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-linkedin" w={24} h={24} className="fill-current" />
           </a>
           <a href="https://github.com/gen-wealth" aria-label="GitHub">
-            <Svg xlinkHref="#social-github" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-github" w={24} h={24} className="fill-current" />
           </a>
           <a href="mailto:hello@genwealth.app" aria-label="Email">
-            <Svg xlinkHref="#social-email" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-email" w={24} h={24} className="fill-current" />
           </a>
         </nav>
       </footer>
--- src/components/FooterSideBar.tsx
+++ src/components/FooterSideBar.tsx
@@ -11,12 +11,12 @@ function FooterSideBar() {
           onClick={scrollToTop}
           className="font-bold"
         >
-          <p className="text-xl">
+          <h5 className="text-xl">
             <FormattedMessage id="genWealth" />
-          </p>
-          <p className="text-xs">
+          </h5>
+          <h6 className="text-xs">
             <FormattedMessage id="tagLine" />
-          </p>
+          </h6>
         </a>
         <p className="text-xs">
           <FormattedMessage id="copyright" />
@@ -25,16 +25,16 @@ function FooterSideBar() {
       <nav>
         <div className="grid grid-flow-col gap-4">
           <a href="https://twitter.com/genwealth_app" aria-label="Twitter">
-            <Svg xlinkHref="#social-twitter" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-twitter" w={24} h={24} className="fill-current" />
           </a>
           <a href="https://www.linkedin.com/company/genwealth-app" aria-label="LinkedIn">
-            <Svg xlinkHref="#social-linkedin" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-linkedin" w={24} h={24} className="fill-current" />
           </a>
           <a href="https://github.com/gen-wealth" aria-label="GitHub">
-            <Svg xlinkHref="#social-github" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-github" w={24} h={24} className="fill-current" />
           </a>
           <a href="mailto:hello@genwealth.app" aria-label="Email">
-            <Svg xlinkHref="#social-email" w={23} h={23} className="fill-current" />
+            <Svg xlinkHref="#social-email" w={24} h={24} className="fill-current" />
           </a>
         </div>
       </nav>
