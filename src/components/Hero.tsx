64ed30c49f9b65a12268acf4baf4dd70ef82d8c5
Ariady Putra <ariady.putra@gmail.com>
2024-04-08 15:44:21 +0700

    Responsive hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,11 @@ function Hero() {
     <div className="hero min-h-screen style-hero bg-[url('https://gen-wealth.github.io/public/hero/Bg.webp')]">
       <div className="hero-overlay bg-opacity-[calc(2/3)]" />
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-lg">
-          <h1 className="mt-10 text-5xl font-bold style1 logo !leading-none">
+        <div className="max-w-screen-xl">
+          <h1 className="mt-[min(5vw,5vh)] text-[min(10vw,10vh)] font-bold style1 logo">
             <FormattedMessage id="genWealth" />
           </h1>
-          <p className="mb-5 style3 logo !leading-tight">
+          <p className="-mt-[min(1.25vw,1.25vh)] mb-[min(2.5vw,2.5vh)] text-[min(5vw,5vh)] style3 logo">
             <FormattedMessage id="tagLine" />
           </p>
           <button onClick={(e) => scrollToElement(e, "our-service")} className="button-secondary">
