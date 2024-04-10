794758cfcd6f966d49bd74d6d5d92514e54112c6
Ariady Putra <ariady.putra@gmail.com>
2024-04-10 18:23:33 +0700

    minor

--- src/components/PrivacyNotice.tsx
+++ src/components/PrivacyNotice.tsx
@@ -25,7 +25,7 @@ function PrivacyNotice() {
         origin-bottom-right ease-in
         ${showNotice ? "pointer-events-auto" : "transition-all duration-[600ms] opacity-0 scale-0 motion-safe:-rotate-180 motion-safe:translate-y-full"}`}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 size-6">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
         <span className="text-center overflow-auto style-mask-y-md px-4 py-2 -mx-4 -my-2">
